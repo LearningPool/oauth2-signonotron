0ebc2b5dc950a06146d264e0ded16a6f4efb3a67
Ryan Lafferty <ryanlafferty@learningpool.com>
2016-09-07 14:22:27 +0100

    Update Signonotron class

--- src/Provider/Signonotron.php
+++ src/Provider/Signonotron.php
@@ -1,28 +1,33 @@
 <?php
-namespace rlafferty05\OAuth2\Client\Provider;
+namespace Learningpool\OAuth2\Client\Provider;
 use \League\OAuth2\Client\Provider\AbstractProvider;
 use \League\OAuth2\Client\Entity\User;
 use \League\OAuth2\Client\Token\AccessToken;
 
 class Signonotron extends AbstractProvider {
-    public $ini = parse_ini_file('signonotron.ini');
+    public $ini;
+
+    public function __construct($options = []) {
+        parent::__construct($options);
+        $this->ini = parse_ini_file('signonotron.ini');
+    }
 
     public function urlAuthorize() {
-        return $ini['authorize_url'];
+        return $this->ini['authorize_url'];
     }
 
     public function urlAccessToken() {
-        return $ini['access_token_url'];
+        return $this->ini['access_token_url'];
     }
 
     public function urlUserDetails(AccessToken $token) {
-        return $ini['access_token_url'] . urlencode($token);
+        return $this->ini['access_token_url'] . urlencode($token);
     }
 
     public function userDetails($response, AccessToken $token) {
         $user = new User;
         $user->uid = $response->uid;
-        $user->name = $response->display_name;
+        $user->name = $response->name;
         $user->email = $response->email;
         return $user;
     }
