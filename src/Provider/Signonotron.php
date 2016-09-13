0b0b17a8947d7bfe7a5566156894bf434355f016
Ryan Lafferty <ryanl@learningpool.com>
2016-09-13 12:27:30 +0100

    Test (#2)
    * Update Signonotron class
    
    * Update composer.json
    
    * Update composer.json
    
    * Update composer.json
    
    * Update composer.json
    
    * Removed ini example from root
    
    * Fixed provider functions, added tests
    
    * Reduced number of config options
    
    * Fixed url function names
    
    * Added README

--- src/Provider/Signonotron.php
+++ src/Provider/Signonotron.php
@@ -1,29 +1,44 @@
 <?php
-namespace rlafferty05\OAuth2\Client\Provider;
+namespace Learningpool\OAuth2\Client\Provider;
 use \League\OAuth2\Client\Provider\AbstractProvider;
 use \League\OAuth2\Client\Entity\User;
 use \League\OAuth2\Client\Token\AccessToken;
 
 class Signonotron extends AbstractProvider {
-    public $ini = parse_ini_file('signonotron.ini');
+    public $ini;
 
     public function urlAuthorize() {
-        return $ini['authorize_url'];
+        return $this->ini['site_url'] . "/oauth/authorize";
     }
-
-    public function urlAccessToken() {
-        return $ini['access_token_url'];
+    public function urlAccessToken(array $params) {
+        return $this->ini['site_url'] . "/oauth/token";
     }
-
     public function urlUserDetails(AccessToken $token) {
-        return $ini['access_token_url'] . urlencode($token);
+        return $this->ini['site_url'] . "/user.json?access_token=" . urlencode($token);
+    }
+    protected function getDefaultScopes() {
+        return $this->defaultScopes;
+    }
+    protected function checkResponse(ResponseInterface $response, $data) {
+        if (!empty($data['error'])) {
+            throw new IdentityProviderException($data['error_description'], $response->getStatusCode(), $response);
+        }
+    }
+    protected function createResourceOwner(array $response, AccessToken $token) {
+        return new SignonotronResourceOwner($response);
+    }
+
+    public function __construct($options = []) {
+        parent::__construct($options);
+        $this->ini = parse_ini_file('signonotron.ini');
     }
 
     public function userDetails($response, AccessToken $token) {
-        $user = new User;
-        $user->uid = $response->uid;
-        $user->name = $response->display_name;
-        $user->email = $response->email;
+        $user = new User();
+        $user->uid = $response->user->uid;
+        $user->name = $response->user->name;
+        $user->email = $response->user->email;
+
         return $user;
     }
 }
